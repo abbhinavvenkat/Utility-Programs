00390d5e4fafb1e2445f106b6228746bf5077d05
Abbhinav Venkat <abbhinavvenkat@gmail.com>
2015-01-07 20:44:40 +0530

    Added Directory Option for Youtube Downloader

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -6,15 +6,29 @@ print "Note: By Default, the Best available format is chosen"
 print "Also, pafy python package is required to run this program"
 
 import pafy
+import os
+import sys
 
 url = raw_input("Enter URL: ")
 video = pafy.new(url)
 
+print " "
 print "Your Video's Name is: %s"  % video.title
- 
+
+path = ""
+path = raw_input("Enter your Path for Saving the File(Press Enter for Current Directory): ")
+
 best = video.getbest()
-print "Downloading from: %s" % best.url
 
-best.download(quiet=False)
+if path != "":
+    if os.path.isdir(path)==False:
+	sys.exit("Invalid Path")
+    else:
+	print ""
+	print "Downloading from: %s" % best.url
+        best.download(filepath = path)
+else:
+    best.download(quiet=False)
 
+print " "
 print "Thank you for using"
